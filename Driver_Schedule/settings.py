51f1c9947e5c8f5a87d3d7367ee0d71c4d8f70be
CooperCharles09 <cooper.charles720@gmail.com>
2024-01-08 22:12:49 +0530

    changes on settings.py

--- Driver_Schedule/settings.py
+++ Driver_Schedule/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,5 +148,3 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
 MODEL_TO_CSV_FILE_NAME = '' 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
