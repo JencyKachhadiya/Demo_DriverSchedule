537dc0dbcd62d6e522385ea05c5c1ed5452bb2e5
CooperCharles09 <cooper.charles720@gmail.com>
2024-01-09 11:45:26 +0530

    some changes

--- Driver_Schedule/deployment.py
+++ Driver_Schedule/deployment.py
@@ -19,8 +19,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ] 
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
